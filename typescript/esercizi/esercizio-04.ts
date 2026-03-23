a39265b834dedcfde2ccd09f400183aaa9f6440d
nicola-montebelli <nicola.montebelli@enaipriminifad.org>
2026-03-23 20:33:30 +0100

    svolti tutti gli esercizi di typescript del prof

--- typescript/esercizi/esercizio-04.ts
+++ typescript/esercizi/esercizio-04.ts
@@ -21,4 +21,39 @@ L'oggetto di risposta della funzione dovrà avere gli stessi campi della seguent
     }
 ```
 PS: restringere la gestione del conteggio ai soli caratteri della tabella ASCII*/
+interface RisultatoAnalisiTesto {
+        testo: string,
+        numeroTotale: number | undefined,
+        numeroVocali: number | undefined,
+        numeroConsonanti: number | undefined,
+        numeroCifre: number | undefined,
+        numeroAltri: number | undefined;
+    };
 
+    let frase: string = "Ecco una stringa da analizzare. Il risultato sarà un'oggetto di un'interfaccia che riporterà i conteggi dei vari caratteri e i numeri compresi tra 0-9";
+
+    function analizzaStringa(stringa: string): RisultatoAnalisiTesto
+    {
+        let conteggioVocali: number | undefined = 0;
+        let conteggioConsonanti: number | undefined = 0;
+        let conteggioCifre: number | undefined = 0;
+        let conteggioAltri: number | undefined = 0;
+        let conteggioTotale: number | undefined = 0;
+
+        conteggioVocali = stringa.match(/[aeiouàèìòùA'E'I'O'U']/g)?.length;
+        conteggioConsonanti = stringa.match(/[b-df-hj-np-tv-z]/g)?.length;
+        conteggioCifre = stringa.match(/\d/g)?.length;
+        conteggioAltri = stringa.match(/[!"€£$%&/()=?'^+\]*\[§°#ç@,;.:_-]/g)?.length;
+        conteggioTotale = stringa.match(/./g)?.length;
+
+        return {
+            testo: stringa,
+            numeroTotale: conteggioTotale,
+            numeroVocali: conteggioVocali,
+            numeroConsonanti: conteggioConsonanti,
+            numeroCifre: conteggioCifre,
+            numeroAltri: conteggioAltri
+        };
+    }
+
+console.log(analizzaStringa(frase));
