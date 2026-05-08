fa5b59043300e1e7925af33246b9278d9456eae7
nicola-montebelli <nicola.montebelli@enaipriminifad.org>
2026-05-08 10:45:03 +0200

    aggiunto HttpClientModule nel file app-module

--- Angular/ricette-app/src/app/app-module.ts
+++ Angular/ricette-app/src/app/app-module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
+import { HttpClientModule, provideHttpClient } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -10,11 +11,13 @@ import { App } from './app';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    //HttpClientModule, --> deprecato, l'alternativa è metterlo nei providers
   ],
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
+    provideHttpClient(),
   ],
   bootstrap: [App]
 })
