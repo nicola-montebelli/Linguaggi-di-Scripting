b1b8308a1ae3ea92054116c1aeba4c17464d24d0
nicola-montebelli <nicola.montebelli@enaipriminifad.org>
2026-05-08 10:35:47 +0200

    aggiunte funzioni di conversione modelli dati nel file recipe-service

--- Angular/ricette-app/src/app/recipe-service.ts
+++ Angular/ricette-app/src/app/recipe-service.ts
@@ -1,6 +1,50 @@
 import { Injectable } from '@angular/core';
+import { IRecipe, IRecipeBackend } from './recipe.model';
 
 @Injectable({
   providedIn: 'root',
 })
-export class RecipeService {}
+export class RecipeService {
+
+  /**
+   * abbiamo la necessità di trasformare il modello dati frontend in backend e viceversa
+   */
+
+  /**
+   * Converte un oggetto IRecipeBackend in un oggetto IRecipe
+   * @param rb oggetto del backend
+   * @returns oggetto usato nel frontend
+   */
+  private mapIRecipeBackendToIRecipe(rb: IRecipeBackend): IRecipe {
+    const r: IRecipe = {
+              id: (rb.id && rb.id.length > 0 ? rb.id : ''),
+              name: rb.nome,
+              category: rb.categoria,
+              image: rb.immagine,
+              description: rb.descrizione,
+              difficulty: rb.difficolta,
+              prepTime: rb.tempoPreparazione,
+            };
+    return r;
+  }
+
+  /**
+   * Converte un oggetto IRecipe in un oggetto IRecipeBackend
+   * @param r oggetto del frontend
+   * @returns oggetto usato nel backend
+   */
+  private mapIRecipeToIRecipeBackend(r: IRecipe): IRecipeBackend {
+    const rb: IRecipeBackend = {
+              nome: r.name,
+              categoria: r.category,
+              descrizione: r.description,
+              difficolta: r.difficulty,
+              immagine: r.image,
+              tempoPreparazione: r.prepTime,
+            };
+    if (r.id.length > 0) {
+      rb.id = r.id;
+    }
+    return rb;
+  }
+}
