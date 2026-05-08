d28039e6bf333597f79cea92e63223e8f0ec5866
nicola-montebelli <nicola.montebelli@enaipriminifad.org>
2026-05-08 12:55:28 +0200

    implementate le funzioni di caricamento della ricetta in recipe-detail.ts e definito l'html in recipe-detail.html

--- Angular/ricette-app/src/app/recipe-detail/recipe-detail.ts
+++ Angular/ricette-app/src/app/recipe-detail/recipe-detail.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { IRecipe } from '../recipe.model';
+import { RecipeService } from '../recipe-service';
 
 @Component({
   selector: 'app-recipe-detail',
@@ -6,4 +9,39 @@ import { Component } from '@angular/core';
   templateUrl: './recipe-detail.html',
   styleUrl: './recipe-detail.css',
 })
-export class RecipeDetail {}
+export class RecipeDetail implements OnInit {
+  recipeId: string | null = null;
+  recipe: IRecipe | null = null;
+  errorMessage: string | null = null;
+
+
+  constructor(private activatedRoute: ActivatedRoute,
+              private recipeService: RecipeService){
+  }
+  ngOnInit(): void {
+    //determina l'id della ricetta
+    this.recipeId = this.activatedRoute.snapshot.paramMap.get('id');
+    console.log('RecipeDetail.ngOnInit(): recipe id=', this.recipeId);
+
+    //se è passato un id, recupero la ricetta
+    this.loadRecipe(this.recipeId);
+  }
+
+  private loadRecipe(id: string | null): void{
+    if(id)
+    {
+      this.recipeService.getRecipe$(id).subscribe({
+        next: (r) => {
+          this.recipe = r;
+          this.errorMessage = null;
+        },
+        error: (err) => {
+          this.errorMessage = err;
+        }
+      });
+    } else{
+      this.errorMessage = 'ID non specificato';
+    }
+  }
+
+}
