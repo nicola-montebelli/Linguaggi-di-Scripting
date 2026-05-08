42c8fae2e126c1508a161b0863860b4fe8dd03fd
nicola-montebelli <nicola.montebelli@enaipriminifad.org>
2026-05-08 11:57:57 +0200

    aggiornata app-routing-module

--- Angular/ricette-app/src/app/app-routing-module.ts
+++ Angular/ricette-app/src/app/app-routing-module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RecipeList } from './recipe-list/recipe-list';
+import { RecipeDetail } from './recipe-detail/recipe-detail';
+import { RecipeEdit } from './recipe-edit/recipe-edit';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: RecipeList},
+  {path: 'recipe/new', component: RecipeEdit},
+  {path: 'recipe/:id', component: RecipeDetail},
+  {path: 'recipe/:id/edit', component: RecipeEdit},
+
+  //redirects to home
+  {path: '**', redirectTo: ''},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
